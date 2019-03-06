72f07d9199819294266b4e7c8d3d86b11a88bd78
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2019-03-06 18:48:31 +0100

    Added prettier questions

--- add-prettier.js
+++ add-prettier.js
@@ -1,4 +1,16 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-console */
+const inquirer = require('inquirer')
+
+const defaults = require('./prettier-defaults')
 const configFile = require('./config-file')
+
+const {
+  mainOptions,
+  propertiesOptions,
+  editPropertiesOptions,
+  fileOptions,
+} = require('./prettier-questions')
 
 const addPrettierToExtends = eslintObj => {
   return Array.isArray(eslintObj.extends)
@@ -8,33 +20,42 @@ const addPrettierToExtends = eslintObj => {
     : 'plugin:prettier/recommended'
 }
 
-const addPrettier = eslintObj => ({
+const addPrettier = (eslintObj, prettier) => ({
   ...eslintObj,
   extends: addPrettierToExtends(eslintObj),
   rules: {
     ...eslintObj.rules,
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'all',
-        semi: false,
-      },
-    ],
+    'prettier/prettier': ['error', prettier],
   },
 })
 
-const writeToEslintRC = () => {
+const questions = () =>
+  inquirer
+    .prompt(mainOptions)
+    .then(({ type }) =>
+      type === 'custom'
+        ? inquirer
+            .prompt(propertiesOptions)
+            .then(({ properties }) =>
+              inquirer.prompt(
+                editPropertiesOptions.filter(
+                  question => properties.indexOf(question.name) !== -1,
+                ),
+              ),
+            )
+        : defaults,
+    )
+    // .then(options => JSON.stringify(options, null, 2))
+    .catch(error => console.error(error))
+
+const writeToEslintRC = async () => {
+  const prettier = await questions()
   console.log('Adding prettier config to eslintrc.json')
 
   const file = configFile.getFilenameForDirectory(process.cwd())
 
-  console.log('file', file)
+  const eslint = configFile.loadConfigFile({ filePath: file })
 
-  const config = configFile.loadConfigFile({ filePath: file })
-
-  console.log('config', config)
-
-  configFile.write(addPrettier(config), file)
+  configFile.write(addPrettier(eslint, prettier), file)
 }
 module.exports = writeToEslintRC
